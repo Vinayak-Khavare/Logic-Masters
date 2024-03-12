afb5fb2a47628c19ae60b2abcd9d2e5a3846bdc1
AparimeyaOp <127532225+AparimeyaOp@users.noreply.github.com>
2024-03-12 11:34:23 +0530

    Update Maximum 69.py

--- Maximum 69.py
+++ Maximum 69.py
@@ -1,3 +1,4 @@
+#Approach using string conversion
 def maximum69Number (num: int):
     a = list(str(num))
     for i in range(len(a)):
@@ -5,3 +6,18 @@ def maximum69Number (num: int):
             a[i] = "9"
             break
     return int(''.join(a))
+
+#Integer Approach
+def maximum69Number (self, num):
+    i = 0
+    temp = num
+    sixind = -1
+    while(temp>0):
+        if(temp%10 == 6):
+            sixind = i
+        temp /= 10
+        i += 1
+    if(sixind == -1):
+        return num
+    else:
+        return(num + 3*(10**sixind))
